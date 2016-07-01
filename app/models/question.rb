7935cbce0a8a29bb0062854532e4fcc1224dc84f
billowkib <erick.bernal.correa@accenture.com>
2016-07-01 17:17:34 -0500

    Implement question delete

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ActiveRecord::Base
 
   include HasGravatar
-
-  has_many :answers
+  validates :email, length: { minimum: 30 }
+  has_many :answers, dependent: :restrict_with_error
 end
